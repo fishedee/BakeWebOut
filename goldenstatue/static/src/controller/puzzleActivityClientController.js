26ec7c8e8406f65c5c2ec707398adae0c380397f
fishedee <306766045@qq.com>
2015-12-11 16:11:38 +0800

    修改分享文案以及旗帜gif

--- goldenstatue/static/src/controller/puzzleActivityClientController.js
+++ goldenstatue/static/src/controller/puzzleActivityClientController.js
@@ -83,7 +83,7 @@ export default Controllers.createClass({
 			var clientName = data.getIn(["clientName"]);
 			this.weixinModel.setShareMessage({
 				title:clientName+"要做蛋糕，就差你帮TA收集啦！快去~",
-				desc:"金像美玫免费礼物送啦！",
+				desc:"金像美玫3,000份面粉“壕”礼相送",
 				imgUrl:'http://goldenstatue.test2.hongbeibang.com/img/logo.jpg',
 				link:this.getLocation()
 			});
